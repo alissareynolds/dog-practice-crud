23f86ac377c5424e41322a5256a910f4650bcc64
Alissa Reynolds <144733508+alissareynolds@users.noreply.github.com>
2024-08-17 15:40:25 -0700

    added an integration test for getDogById method

--- src/test/java/com/example/animal/controller/DogControllerIntegrationTest.java
+++ src/test/java/com/example/animal/controller/DogControllerIntegrationTest.java
@@ -4,6 +4,7 @@ import com.example.animal.model.Dog;
 import com.example.animal.service.DogService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.List;
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -49,6 +51,14 @@ public class DogControllerIntegrationTest {
     public void getAllDogs() throws Exception {
         mvc.perform(MockMvcRequestBuilders
                 .get("/api/dogs").accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk());
+    }
+
+    @Test
+    public void getDogById() throws Exception {
+        Mockito.when(mockDogService.getById(UUID.fromString("59c47568-fde0-4dd7-9aef-03db6a962810"))).thenReturn(new Dog());
+        mvc.perform(MockMvcRequestBuilders
+                .get("/api/dogs/59c47568-fde0-4dd7-9aef-03db6a962810").accept(MediaType.APPLICATION_JSON)
         ).andExpect(status().isOk());
     }
 }
